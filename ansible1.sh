13792af93e4937150343a0f2200477ea8f8c4a94
chaitu <chaitus.ch09@gmail.com>
2024-04-07 18:38:55 -0700

    GOCD pipeline sample testing

--- ansible1.sh
+++ ansible1.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i 172.31.94.153, -e ansible_user=ec2-user -e ansible_password=DevOps321 07-tags.yml
+ansible-playbook -i 172.31.90.168, -e ansible_user=ec2-user -e ansible_password=DevOps321 07-tags.yml
